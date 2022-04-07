5fc4bf0e78bb75f290bbbe28f7889215a9cbd747
Stephen Elliott <stephenmatthewelliott@gmail.com>
2022-04-06 22:58:01 -0500

    added dependecies and function so user can choose a category to go to

---
+++ server.js
@@ -0,0 +1,80 @@
+//start with the dependencies
+const mysql = require("mysql2");
+const inquirer = require("inquirer");
+require("console.table");
+
+const questions = [
+    {
+        type: "list",
+        name: "task",
+        message: "Would you like to do?",
+        choices: [
+            "View Employees",
+            "View Employees by Department",
+            "Add Employee",
+            "Remove Employees",
+            "Update Employee Role",
+            "Add Role",
+            "End"
+        ]  
+    }
+]
+
+
+async function choicePrompt() {
+    const result = await inquirer.prompt(questions);
+    switch (result.task) {
+        case "View Employees":
+          viewEmployee();
+          break;
+
+        case "View Employees by Department":
+          viewEmployeeByDepartment();
+          break;
+      
+        case "Add Employee":
+          addEmployee();
+          break;
+
+        case "Remove Employees":
+          removeEmployees();
+          break;
+
+        case "Update Employee Role":
+          updateEmployeeRole();
+          break;
+
+        case "Add Role":
+          addRole();
+          break;
+
+        case "End":
+          connection.end();
+          break;
+    }
+}
+
+function viewEmployee() {
+    console.log("Viewing employees\n");
+  
+    let query =
+      `SELECT e.id, e.first_name, e.last_name, r.title, d.name AS department, r.salary, CONCAT(m.first_name, ' ', m.last_name) AS manager
+        FROM employee e
+    LEFT JOIN role r
+      ON e.role_id = r.id
+    LEFT JOIN department d
+    ON d.id = r.department_id
+    LEFT JOIN employee m
+      ON m.id = e.manager_id`
+   
+    connection.query(query, function (err, res) {
+        if (err) throw err;
+    
+        console.table(res);
+        console.log("Employees viewed!\n");
+    
+        choicePrompt();
+    });
+    
+}
+      
